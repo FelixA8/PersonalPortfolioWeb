7a54d24552661423e398da86246dac14862ef156
FelixAnderson <felix.anderson1504@gmail.com>
2024-05-04 23:34:23 +0700

    update 05-05-2024 2

--- src/ui/svg/expo-logo.tsx
+++ src/ui/svg/expo-logo.tsx
@@ -5,7 +5,7 @@ const ExpoLogo = () => {
     <IconComponent>
       <svg
         role="img"
-        color="#FFF"
+        fill="#FFF"
         viewBox="0 0 24 24"
         xmlns="http://www.w3.org/2000/svg"
       >

550f9ceb3b6e2895fc4636d59ffdd52bcb07010a
Felix Anderson <121009387+FelixA8@users.noreply.github.com>
2025-03-23 15:00:13 +0700

    update: fixing some wrong imports

--- src/app/learning-journey/[slug]/page.tsx
+++ src/app/learning-journey/[slug]/page.tsx
@@ -1,6 +1,5 @@
 "use client";
 import JourneySlug from "@/src/ui/section-journeyslug";
-import PastProjectSlug from "@/src/ui/section-projectslug";
 import { usePathname, useSearchParams } from "next/navigation";
 
 const Page = () => {
